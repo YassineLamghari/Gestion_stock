02fa4605e3c7cd84a8a19b6379fbc2b6ef67f358
Yassine Lamghari <yasslamghari123@gmail.com>
2024-03-31 22:36:52 +0000

    v22.36

--- Dashboard/index.php
+++ Dashboard/index.php
@@ -528,132 +528,56 @@ include("../securite/cnx.php");
                                 </div>
                                 <div class="intro-y overflow-auto lg:overflow-visible mt-8 sm:mt-0">
                                     <table class="table table-report sm:mt-2">
+                                    
                                         <thead>
                                             <tr>
-                                                <th class="whitespace-nowrap">IMAGES</th>
+                                                <th class="whitespace-nowrap">#REFERENCE</th>
                                                 <th class="whitespace-nowrap">PRODUCT NAME</th>
                                                 <th class="text-center whitespace-nowrap">STOCK</th>
+                                                <th class="text-center whitespace-nowrap">PRICE</th>
                                                 <th class="text-center whitespace-nowrap">STATUS</th>
                                                 <th class="text-center whitespace-nowrap">ACTIONS</th>
                                             </tr>
                                         </thead>
                                         <tbody>
+                                            <?php 
+                                                $req_product="SELECT * FROM product";
+                                                $res_product=mysqli_query($cnx,$req_product);
+                                                $data_product=mysqli_fetch_array($res_product);  
+                                                
+                                                while($row_product=mysqli_fetch_array($res_product)){ 
+                                            ?>
                                             <tr class="intro-x">
                                                 <td class="w-40">
                                                     <div class="flex">
-                                                        <div class="w-10 h-10 image-fit zoom-in">
-                                                            <img alt="Midone - HTML Admin Template" class="tooltip rounded-full" src="../dist/images/preview-12.jpg" title="Uploaded at 3 June 2020">
-                                                        </div>
-                                                        <div class="w-10 h-10 image-fit zoom-in -ml-5">
-                                                            <img alt="Midone - HTML Admin Template" class="tooltip rounded-full" src="../dist/images/preview-6.jpg" title="Uploaded at 6 August 2020">
-                                                        </div>
-                                                        <div class="w-10 h-10 image-fit zoom-in -ml-5">
-                                                            <img alt="Midone - HTML Admin Template" class="tooltip rounded-full" src="../dist/images/preview-13.jpg" title="Uploaded at 3 July 2022">
+                                                        <div class="text-center mx-auto font-medium">
+                                                            <?php echo $row_product['id']; ?>
                                                         </div>
                                                     </div>
                                                 </td>
                                                 <td>
-                                                    <a href="" class="font-medium whitespace-nowrap">Samsung Galaxy S20 Ultra</a> 
-                                                    <div class="text-slate-500 text-xs whitespace-nowrap mt-0.5">Smartphone &amp; Tablet</div>
-                                                </td>
-                                                <td class="text-center">88</td>
-                                                <td class="w-40">
-                                                    <div class="flex items-center justify-center text-success"> <i data-lucide="check-square" class="w-4 h-4 mr-2"></i> Active </div>
-                                                </td>
-                                                <td class="table-report__action w-56">
-                                                    <div class="flex justify-center items-center">
-                                                        <a class="flex items-center mr-3" href=""> <i data-lucide="check-square" class="w-4 h-4 mr-1"></i> Edit </a>
-                                                        <a class="flex items-center text-danger" href=""> <i data-lucide="trash-2" class="w-4 h-4 mr-1"></i> Delete </a>
-                                                    </div>
-                                                </td>
-                                            </tr>
-                                            <tr class="intro-x">
-                                                <td class="w-40">
-                                                    <div class="flex">
-                                                        <div class="w-10 h-10 image-fit zoom-in">
-                                                            <img alt="Midone - HTML Admin Template" class="tooltip rounded-full" src="../dist/images/preview-15.jpg" title="Uploaded at 18 October 2022">
-                                                        </div>
-                                                        <div class="w-10 h-10 image-fit zoom-in -ml-5">
-                                                            <img alt="Midone - HTML Admin Template" class="tooltip rounded-full" src="../dist/images/preview-5.jpg" title="Uploaded at 15 June 2021">
-                                                        </div>
-                                                        <div class="w-10 h-10 image-fit zoom-in -ml-5">
-                                                            <img alt="Midone - HTML Admin Template" class="tooltip rounded-full" src="../dist/images/preview-5.jpg" title="Uploaded at 12 June 2020">
-                                                        </div>
-                                                    </div>
-                                                </td>
-                                                <td>
-                                                    <a href="" class="font-medium whitespace-nowrap">Oppo Find X2 Pro</a> 
-                                                    <div class="text-slate-500 text-xs whitespace-nowrap mt-0.5">Smartphone &amp; Tablet</div>
-                                                </td>
-                                                <td class="text-center">94</td>
-                                                <td class="w-40">
-                                                    <div class="flex items-center justify-center text-danger"> <i data-lucide="check-square" class="w-4 h-4 mr-2"></i> Inactive </div>
-                                                </td>
-                                                <td class="table-report__action w-56">
-                                                    <div class="flex justify-center items-center">
-                                                        <a class="flex items-center mr-3" href=""> <i data-lucide="check-square" class="w-4 h-4 mr-1"></i> Edit </a>
-                                                        <a class="flex items-center text-danger" href=""> <i data-lucide="trash-2" class="w-4 h-4 mr-1"></i> Delete </a>
-                                                    </div>
-                                                </td>
-                                            </tr>
-                                            <tr class="intro-x">
-                                                <td class="w-40">
-                                                    <div class="flex">
-                                                        <div class="w-10 h-10 image-fit zoom-in">
-                                                            <img alt="Midone - HTML Admin Template" class="tooltip rounded-full" src="../dist/images/preview-5.jpg" title="Uploaded at 5 September 2020">
-                                                        </div>
-                                                        <div class="w-10 h-10 image-fit zoom-in -ml-5">
-                                                            <img alt="Midone - HTML Admin Template" class="tooltip rounded-full" src="../dist/images/preview-14.jpg" title="Uploaded at 23 August 2021">
-                                                        </div>
-                                                        <div class="w-10 h-10 image-fit zoom-in -ml-5">
-                                                            <img alt="Midone - HTML Admin Template" class="tooltip rounded-full" src="../dist/images/preview-9.jpg" title="Uploaded at 27 July 2020">
-                                                        </div>
-                                                    </div>
-                                                </td>
-                                                <td>
-                                                    <a href="" class="font-medium whitespace-nowrap">Nike Tanjun</a> 
+                                                    <a href="" class="font-medium whitespace-nowrap"><?php echo $row_product['libelle'] ?></a> 
                                                     <div class="text-slate-500 text-xs whitespace-nowrap mt-0.5">Sport &amp; Outdoor</div>
                                                 </td>
-                                                <td class="text-center">103</td>
+                                                <td class="text-center"><?php echo $row_product['Qte'] ?></td>
+                                                <td class="text-center"><?php echo $row_product['Prix'] ?></td>
                                                 <td class="w-40">
-                                                    <div class="flex items-center justify-center text-success"> <i data-lucide="check-square" class="w-4 h-4 mr-2"></i> Active </div>
+                                                    <?php 
+                                                        if ($row_product['Qte'] <= 10) {
+                                                            echo '<div class="flex items-center justify-center text-danger"> <i data-lucide="check-square" class="w-4 h-4 mr-2"></i> Out Stock </div>';
+                                                        } else {
+                                                            echo '<div class="flex items-center justify-center text-success"> <i data-lucide="check-square" class="w-4 h-4 mr-2"></i> In Stock </div>';
+                                                        }
+                                                        ?>
                                                 </td>
                                                 <td class="table-report__action w-56">
                                                     <div class="flex justify-center items-center">
-                                                        <a class="flex items-center mr-3" href=""> <i data-lucide="check-square" class="w-4 h-4 mr-1"></i> Edit </a>
-                                                        <a class="flex items-center text-danger" href=""> <i data-lucide="trash-2" class="w-4 h-4 mr-1"></i> Delete </a>
+                                                        <a class="flex items-center mr-3" href="../product/modifier_product.php?modifier_product=<?php echo $row_product['id'] ?>"> <i data-lucide="check-square" class="w-4 h-4 mr-1"></i> Edit </a>
+                                                        <a class="flex items-center text-danger" href="../product/product_list.php?supprimer_product=<?php echo $row_product['id'] ?>" onclick="confirm(javascript:;);" data-tw-toggle="modal" data-tw-target="#delete-confirmation-modal"> <i data-lucide="trash-2" class="w-4 h-4 mr-1"></i> Delete </a>
                                                     </div>
                                                 </td>
                                             </tr>
-                                            <tr class="intro-x">
-                                                <td class="w-40">
-                                                    <div class="flex">
-                                                        <div class="w-10 h-10 image-fit zoom-in">
-                                                            <img alt="Midone - HTML Admin Template" class="tooltip rounded-full" src="../dist/images/preview-12.jpg" title="Uploaded at 21 May 2020">
-                                                        </div>
-                                                        <div class="w-10 h-10 image-fit zoom-in -ml-5">
-                                                            <img alt="Midone - HTML Admin Template" class="tooltip rounded-full" src="../dist/images/preview-6.jpg" title="Uploaded at 4 February 2022">
-                                                        </div>
-                                                        <div class="w-10 h-10 image-fit zoom-in -ml-5">
-                                                            <img alt="Midone - HTML Admin Template" class="tooltip rounded-full" src="../dist/images/preview-14.jpg" title="Uploaded at 24 July 2020">
-                                                        </div>
-                                                    </div>
-                                                </td>
-                                                <td>
-                                                    <a href="" class="font-medium whitespace-nowrap">Apple MacBook Pro 13</a> 
-                                                    <div class="text-slate-500 text-xs whitespace-nowrap mt-0.5">PC &amp; Laptop</div>
-                                                </td>
-                                                <td class="text-center">164</td>
-                                                <td class="w-40">
-                                                    <div class="flex items-center justify-center text-success"> <i data-lucide="check-square" class="w-4 h-4 mr-2"></i> Active </div>
-                                                </td>
-                                                <td class="table-report__action w-56">
-                                                    <div class="flex justify-center items-center">
-                                                        <a class="flex items-center mr-3" href=""> <i data-lucide="check-square" class="w-4 h-4 mr-1"></i> Edit </a>
-                                                        <a class="flex items-center text-danger" href=""> <i data-lucide="trash-2" class="w-4 h-4 mr-1"></i> Delete </a>
-                                                    </div>
-                                                </td>
-                                            </tr>
+                                            <?php } ?>
                                         </tbody>
                                     </table>
                                 </div>
